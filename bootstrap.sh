b4da42a364a4aa459ad6c63a3d115fb644d04d3f
Dmitriy Kuminov <coding@dmik.org>
2015-01-19 19:24:20 +0000

    help2man: Hack bootstrap to overcome ash bug on OS/2.
    git-svn-id: http://svn.netlabs.org/repos/ports/help2man/trunk@980 8d6eb4b1-0faa-de11-b929-00e081727c95

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,7 +38,19 @@ run sed \
     -e '/^	\($(MAKE)\)/d' \
     -i bootstrap.mak
 
+# OS/2 hot fix on:
+# configure.ac runs help2man.PL to get the script vesion. On OS/2 autoconf uses
+# ash (sh.exe) as the shell. However, the current version of ash can't properly
+# handle the script interprer spec when it is given using the full path. See
+# http://trac.netlabs.org/ports/ticket/42 for details. The fix is to temporarily
+# hack help2man.PL to remove the path from the interpreter spec.
+run mv help2man.PL help2man.PL.orig
+run sed -e '1 s/^#!.*\/perl/#!perl/' help2man.PL.orig > help2man.PL
+
 run make -f bootstrap.mak maint-prep
+
+# OS/2 hot fix off:
+run mv help2man.PL.orig help2man.PL
 
 # Cleanup
 
